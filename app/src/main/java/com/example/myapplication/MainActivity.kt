430d0561d1c7fbfffbce8de8ef8aea1e5c21e334
Eduardo-Pereira-Santos <eduardounivap2025@gmail.com>
2025-03-23 22:06:18 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,3 @@
-package com.example.calculodeimc
-
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -28,8 +26,19 @@ class MainActivity : AppCompatActivity() {
         val altura = altura.text.toString().toFloatOrNull()
 
         if (peso != null && altura != null && altura > 0) {
-            val IMC = peso / (altura * altura)
-            resultado.text = "IMC: %.2f\n$IMC".format(IMC)
+            val imc = peso / (altura * altura)
+
+            // Classificação do IMC
+            val classificacao = when {
+                imc < 18.5 -> "Abaixo do peso"
+                imc in 18.5..24.9 -> "Peso normal"
+                imc in 25.0..29.9 -> "Sobrepeso"
+                imc in 30.0..34.9 -> "Obesidade grau 1"
+                imc in 35.0..39.9 -> "Obesidade grau 2"
+                else -> "Obesidade grau 3 (obesidade mórbida)"
+            }
+
+            resultado.text = "IMC: %.2f\n$classificacao".format(imc)
         } else {
             resultado.text = "Insira somente valores válidos!"
         }
